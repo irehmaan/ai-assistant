df9906f11d7963bd91ba792b7d23c68c7141bc33
Imdadul Rehmaan <rehmaankhan287@gmail.com>
2023-12-08 13:04:56 +0530

    added comments in Contants.dart

--- lib/configs/configs.dart
+++ lib/configs/configs.dart
@@ -4,8 +4,7 @@ class Configs {
   /*
   Obtain your OpenAI API key and paste it below.
   */
-  static const OpenAIKey =
-      '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';
+  static const OpenAIKey = 'YOUR_OPEN_API_KEY';
 
   static String getTrend(String query) {
     return "https://itunes.apple.com/search?term=$query&entity=musicTrack&limit =50";
